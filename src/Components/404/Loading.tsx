e9fe4257372b37274393b58ee8296635ee1cf400
The Khiem <lekhiem2002@gmail.com>
2022-03-28 20:20:10 +0700

    update

--- src/Components/404/Loading.tsx
+++ src/Components/404/Loading.tsx
@@ -4,11 +4,21 @@ Loading.propTypes = {};
 
 function Loading() {
   const [counter, setCounter] = useState<any>(0);
+  const [active, setActive] = useState<any>(false);
+  const [active2, setActive2] = useState<any>(false);
+
   useEffect(() => {
     if (counter < 100) {
       const timer = setInterval(() => {
         setCounter(counter * 2 + 5);
       }, 40);
+
+      setTimeout(() => {
+        setActive(true);
+        setTimeout(() => {
+          setActive2(true);
+        }, 1000);
+      }, 1500);
 
       return () => {
         clearInterval(timer);
@@ -18,10 +28,14 @@ function Loading() {
   return (
     <body
       className="home page-template page-template-template page-template-HomePage page-template-templateHomePage-php page page-id-92 mll_custom_loader unscrollable noscroll"
-      style={{ display: "block" }}
+      style={active2 ? { display: "none" } : { display: "block" }}
     >
       <div id="mll_preloader">
-        <div className="mll_preloader_top active-top-loader">
+        <div
+          className={
+            active ? "mll_preloader_top" : "mll_preloader_top active-top-loader"
+          }
+        >
           <div
             id="mll_preloader_loading_percentage"
             style={{ left: `${counter}%` }}
@@ -30,9 +44,19 @@ function Loading() {
             <span>%</span>
           </div>
         </div>
-        <div className="mll_preloader_bottom active-bottom-loader"></div>
         <div
-          id="mll_preloader_loading_line"
+          className={
+            active
+              ? "mll_preloader_bottom"
+              : "mll_preloader_bottom active-bottom-loader"
+          }
+        ></div>
+        <div
+          id={
+            active
+              ? "mll_preloader_loading_percentage"
+              : "mll_preloader_loading_line"
+          }
           style={{ width: `${counter}%` }}
         ></div>
       </div>
--- src/Layout/Home/Home.tsx
+++ src/Layout/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Loading from "../../Components/404/Loading";
 import Header from "../../Components/Header/Header";
 
 Home.propTypes = {};
@@ -6,6 +7,7 @@ Home.propTypes = {};
 function Home() {
   return (
     <>
+      <Loading />
       <Header />
     </>
   );
